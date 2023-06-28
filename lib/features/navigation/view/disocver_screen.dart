3f5a02005765adba4362ca7adf4ed390344bbc99
limnull <m.3098.off@gmail.com>
2023-06-28 21:02:48 +0300

    Создание экрана discover

--- lib/features/navigation/view/disocver_screen.dart
+++ lib/features/navigation/view/disocver_screen.dart
@@ -5,8 +5,41 @@ class DiscoverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("DISCOVER"),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return Column(
+          children: [
+            ListTile(
+              title: Text('meme$index'),
+              subtitle: Text('poular tag'),
+              leading: Icon(
+                Icons.g_mobiledata,
+                size: 50,
+              ),
+              trailing: Icon(Icons.arrow_right),
+            ),
+            SizedBox(
+              height: 160,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: Container(
+                      color: Colors.red,
+                      height: 160,
+                      width: 90,
+                      child: Center(child: Text("$index")),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
