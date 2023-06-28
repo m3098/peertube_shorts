0b8d131f065bd3f3e92d000775942234d6a6a0df
limnull <m.3098.off@gmail.com>
2023-06-28 12:44:24 +0300

    Добавлен функционал BottomNavigationBar

--- lib/features/navigation/view/navigation_screen.dart
+++ lib/features/navigation/view/navigation_screen.dart
@@ -1,40 +1,51 @@
 import 'package:flutter/material.dart';
 import 'view.dart';
 
-class NavigationScreen extends StatelessWidget {
+class NavigationScreen extends StatefulWidget {
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  int _currentScreeen = 0;
+  List<Widget> _screens = [
+    HomeScreen(),
+    DiscoverScreen(),
+    UploadScreen(),
+    InboxScreen(),
+    AccountScreen()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
-      body: HomeScreen(),
+      extendBody: _currentScreeen == 0,
+      body: _screens[_currentScreeen],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: const Color.fromARGB(45, 0, 0, 0),
+        currentIndex: _currentScreeen,
+        onTap: (index) => setState(() {
+          _currentScreeen = index;
+        }),
         items: [
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.search),
             label: 'Discover',
           ),
           BottomNavigationBarItem(
-            icon: SizedBox(
-              height: 30,
-              width: 30,
-              child: FloatingActionButton(
-                onPressed: () {},
-                child: const Icon(Icons.add),
-              ),
-            ),
+            icon: const Icon(Icons.add),
             label: '',
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.mail),
             label: 'Inbox',
           ),
-          const BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.account_circle),
             label: 'Account',
           ),
--- lib/features/navigation/view/view.dart
+++ lib/features/navigation/view/view.dart
@@ -1,2 +1,6 @@
 export 'navigation_screen.dart';
 export 'home_screen.dart';
+export 'account_screen.dart';
+export 'disocver_screen.dart';
+export 'inbox_screen.dart';
+export 'upload_screen.dart';
